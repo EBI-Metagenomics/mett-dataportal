a4b886360733eb9b43563d4fe77b0bcbb42e163b
vikasg11 <vikasg@ebi.ac.uk>
2024-09-17 11:18:11 +0100

    intermediate state

--- dataportal-app/src/services/speciesService.ts
+++ dataportal-app/src/services/speciesService.ts
@@ -1,4 +1,5 @@
 import { getData } from './api';
+import apiInstance from "./apiInstance";
 
 // Define a function to fetch species data
 export const fetchSpeciesList = async (): Promise<any> => {
@@ -8,4 +9,12 @@ export const fetchSpeciesList = async (): Promise<any> => {
     console.error('Error fetching species list:', error);
     throw error;
   }
+};
+
+export const fetchIsolateList = async (speciesId?: string) => {
+  const url = speciesId
+    ? `/api/isolates?species=${speciesId}` // Fetch isolates by species
+    : '/api/isolates'; // Fetch all isolates
+  const response = await apiInstance.get(url);
+  return response.data;
 };
