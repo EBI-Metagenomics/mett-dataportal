608fd6e9c4ce321b28d74235111332c46f8dd790
vikasg11 <vikasg@ebi.ac.uk>
2024-11-08 13:53:23 +0000

    deployment configuration in progress

--- dataportal-app/src/services/apiInstance.ts
+++ dataportal-app/src/services/apiInstance.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const apiInstance = axios.create({
-  baseURL: 'http://localhost:8000/api', // Update with your actual base URL
+  baseURL: process.env.REACT_APP_API_BASE_URL,
   headers: {
     'Content-Type': 'application/json',
   },
