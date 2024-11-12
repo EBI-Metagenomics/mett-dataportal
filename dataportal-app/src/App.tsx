6b3943b4146c4163453a60d1b6c8cade31000437
vikasg11 <vikasg@ebi.ac.uk>
2024-11-12 09:53:52 +0000

    footer issue fixed

--- dataportal-app/src/App.tsx
+++ dataportal-app/src/App.tsx
@@ -4,29 +4,35 @@ import Header from './components/organisms/Header';
 import HomePage from './components/pages/HomePage';
 import GeneViewerPage from './components/pages/GeneViewerPage';
 import ErrorBoundary from "@components/atoms/ErrorBoundary";
+import Footer from "@components/organisms/Footer";
 
 const App: React.FC = () => {
     return (
-        <Router>
-            <Header/>
-            <main
-                className="vf-body | vf-stack vf-stack--200"
-                style={{'--vf-body-width': '80em'} as React.CSSProperties}
-            >
-                <Routes>
-                    <Route path="/" element={<HomePage />} />
-                    {/* Route for gene and genome */}
-                    <Route path="/gene-viewer/gene/:geneId/genome/:genomeId" element={<GeneViewerPage />} />
-                    {/* Route for genome only */}
-                    <Route path="/gene-viewer/genome/:genomeId" element={
-                        <ErrorBoundary>
-                            <GeneViewerPage />
-                        </ErrorBoundary>
-                    } />
-                </Routes>
-            </main>
-            {/*<Footer />*/}
-        </Router>
+        <div style={{display: 'flex', flexDirection: 'column', minHeight: '100vh'}}>
+            <Router>
+                <Header/>
+                <main
+                    className="vf-body | vf-stack vf-stack--200"
+                    style={{
+                        '--vf-body-width': '80em',
+                        paddingBottom: '200px',
+                    } as React.CSSProperties}
+                >
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        {/* Route for gene and genome */}
+                        <Route path="/gene-viewer/gene/:geneId/genome/:genomeId" element={<GeneViewerPage/>}/>
+                        {/* Route for genome only */}
+                        <Route path="/gene-viewer/genome/:genomeId" element={
+                            <ErrorBoundary>
+                                <GeneViewerPage/>
+                            </ErrorBoundary>
+                        }/>
+                    </Routes>
+                </main>
+                <Footer/>
+            </Router>
+        </div>
     );
 };
 
