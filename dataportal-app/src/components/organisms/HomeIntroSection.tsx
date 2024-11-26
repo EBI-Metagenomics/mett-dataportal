30478911da730e55a69e8cc0dc1212dd50d2cb15
vikasg11 <vikasg@ebi.ac.uk>
2024-11-26 08:58:11 +0000

    header card removed

--- dataportal-app/src/components/organisms/HomeIntroSection.tsx
+++ dataportal-app/src/components/organisms/HomeIntroSection.tsx
@@ -26,28 +26,6 @@ const HomeIntroSection: React.FC = () => {
                 functional annotations (including biosynthetic gene clusters, carbohydrate active enzymes, etc).
               </p>
             </div>
-            <div className={`vf-grid__col--span-3 ${styles.vfGridColSpan3}`}>
-              <article className="vf-card vf-card--brand vf-card--bordered">
-                <div className="vf-card__content | vf-stack vf-stack--400">
-                  <h3 className="vf-card__heading"><a className="vf-card__link"
-                                                      href="JavaScript:Void(0);">A Bordered Card
-                    Heading
-                    <svg aria-hidden="true"
-                         className="vf-card__heading__icon | vf-icon vf-icon-arrow--inline-end"
-                         width="1em" height="1em" xmlns="http://www.w3.org/2000/svg">
-                      <path
-                          d="M0 12c0 6.627 5.373 12 12 12s12-5.373 12-12S18.627 0 12 0C5.376.008.008 5.376 0 12zm13.707-5.209l4.5 4.5a1 1 0 010 1.414l-4.5 4.5a1 1 0 01-1.414-1.414l2.366-2.367a.25.25 0 00-.177-.424H6a1 1 0 010-2h8.482a.25.25 0 00.177-.427l-2.366-2.368a1 1 0 011.414-1.414z"
-                          fill="currentColor" fillRule="nonzero"></path>
-                    </svg>
-                  </a></h3>
-                  <p className="vf-card__subheading">With sub–heading</p>
-                  <p className="vf-card__text">Lorem ipsum dolor sit amet, consectetur <a
-                      href="JavaScript:Void(0);"
-                      className="vf-card__link">adipisicing elit</a>. Sapiente harum, omnis provident
-                    saepe aut eius aliquam sequi fugit incidunt reiciendis, mollitia quos?</p>
-                </div>
-              </article>
-            </div>
           </div>
           <div>&nbsp;</div>
           <hr className="vf-divider"></hr>
