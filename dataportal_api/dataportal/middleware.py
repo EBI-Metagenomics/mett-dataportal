0da77dca7d2e3be727c71796a12b458b62d6a9b4
vikasg11 <vikasg@ebi.ac.uk>
2024-12-03 13:39:05 +0000

    public domain mapping changes

--- dataportal_api/dataportal/middleware.py
+++ dataportal_api/dataportal/middleware.py
@@ -1,20 +1,37 @@
-from datetime import datetime
-from ninja.middleware import Middleware
 import logging
+from datetime import datetime
+from django.middleware.common import MiddlewareMixin
 
 logger = logging.getLogger(__name__)
 
 
-class LoggingMiddleware(Middleware):
-    def __init__(self):
-        pass
+class RemoveCOOPHeaderMiddleware(MiddlewareMixin):
+    def process_response(self, request, response):
+        # Remove the Cross-Origin-Opener-Policy header if it exists
+        if "Cross-Origin-Opener-Policy" in response:
+            del response["Cross-Origin-Opener-Policy"]
+        return response
 
-    async def __call__(self, request, call_next):
+
+class LoggingMiddleware(MiddlewareMixin):
+    def process_request(self, request):
+        # Log the request details
         start_time = datetime.now()
-        logger.info(f"API accessed: {request.method} {request.url} at {start_time}")
-        response = await call_next(request)
-        end_time = datetime.now()
+        request.start_time = start_time
         logger.info(
-            f"API response: {response.status_code} {request.url} took {end_time - start_time}"
+            f"API accessed: {request.method} {request.get_full_path()} at {start_time}"
+        )
+
+    def process_response(self, request, response):
+        # Log the response details
+        end_time = datetime.now()
+        start_time = getattr(request, "start_time", None)
+        if start_time:
+            duration = end_time - start_time
+        else:
+            duration = "unknown"
+
+        logger.info(
+            f"API response: {response.status_code} {request.get_full_path()} took {duration}"
         )
         return response
--- dataportal_api/dataportal/settings.py
+++ dataportal_api/dataportal/settings.py
@@ -1,6 +1,10 @@
 import os
 import sys
 from pathlib import Path
+import logging
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -41,10 +45,11 @@ LOGGING = {
     },
 }
 
-ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 CSRF_TRUSTED_ORIGINS = [
     f"https://{os.environ.get('DATA_PORTAL_URL', '127.0.0.1')}",
     f"http://{os.environ.get('DATA_PORTAL_URL', '127.0.0.1')}",
+    "http://www.gut-microbes.org",
+    "http://api.gut-microbes.org",
 ]
 
 # Application definition
@@ -73,6 +78,8 @@ MIDDLEWARE = [
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
+
+MIDDLEWARE += ["dataportal.middleware.RemoveCOOPHeaderMiddleware"]
 
 if DEBUG:
     MIDDLEWARE.append("debug_toolbar.middleware.DebugToolbarMiddleware")
@@ -150,13 +157,29 @@ if DEBUG:
         "127.0.0.1",
     ]
 
-CORS_ALLOWED_ORIGINS = [
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-]
+ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "localhost").split(",")
 
-CORS_ALLOW_ALL_ORIGINS = True  # todo remove later
-APPEND_SLASH = False  # todo verify and remove
+CORS_ALLOWED_ORIGINS = [f"http://{h}" for h in ALLOWED_HOSTS] + [
+    f"https://{h}" for h in ALLOWED_HOSTS
+]
+if DEBUG:
+    CORS_ALLOWED_ORIGINS += [
+        "http://localhost:3000",
+        "http://127.0.0.1:3000",
+    ]
+logger.info("ALLOWED_HOSTS: %s", ALLOWED_HOSTS)
+logger.info("CORS_ALLOWED_ORIGINS: %s", CORS_ALLOWED_ORIGINS)
+
+# if not DEBUG:
+#     SECURE_SSL_REDIRECT = True
+#     CSRF_COOKIE_SECURE = True
+#     SESSION_COOKIE_SECURE = True
+# else:
+#     SECURE_SSL_REDIRECT = False
+
+CORS_ALLOW_PRIVATE_NETWORK = True
+
+APPEND_SLASH = False
 
 DEFAULT_LIMIT = 10
 ASSEMBLY_FTP_PATH = os.environ.get(
