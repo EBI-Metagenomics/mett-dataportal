bc9c72f94df694420ca99ce6c629d0356a1011cc
vikasg11 <vikasg@ebi.ac.uk>
2024-12-17 16:45:42 +0000

    essentiality track working basic

--- dataportal-app/src/components/organisms/GeneViewer/tracks.ts
+++ dataportal-app/src/components/organisms/GeneViewer/tracks.ts
@@ -1,8 +1,10 @@
 import {GenomeMeta} from "../../../interfaces/Genome";
 
-const getTracks = (genomeMeta: GenomeMeta, gffBaseUrl: string) => [
+const getTracks = (genomeMeta: GenomeMeta, gffBaseUrl: string) => {
+    const tracks = [];
+
     // Structural Annotation Track
-    {
+    tracks.push({
         type: 'FeatureTrack',
         trackId: 'structural_annotation',
         name: 'Structural Annotation',
@@ -55,46 +57,53 @@ const getTracks = (genomeMeta: GenomeMeta, gffBaseUrl: string) => [
                 showLabels: true,
             },
         ],
-    },
+    });
 
-    // Essentiality Track
-    {
-        type: 'FeatureTrack',
-        trackId: 'essentiality_annotation',
-        name: 'Essentiality Annotation',
-        assemblyNames: [genomeMeta.assembly_name],
-        category: ['Essentiality'],
-        adapter: {
-            type: 'Gff3TabixAdapter',
-            gffGzLocation: {
-                uri: `${gffBaseUrl}/${genomeMeta.isolate_name}/essentiality/${genomeMeta.isolate_name}_essentiality.gff3.gz`,
-            },
-            index: {
-                location: {
-                    uri: `${gffBaseUrl}/${genomeMeta.isolate_name}/essentiality/${genomeMeta.isolate_name}_essentiality.gff3.gz.tbi`,
+    // Conditionally add the Essentiality Track for type strains
+    if (genomeMeta.type_strain) {
+        tracks.push({
+            type: 'FeatureTrack',
+            trackId: 'essentiality_annotation',
+            name: 'Essentiality Annotation',
+            assemblyNames: [genomeMeta.assembly_name],
+            category: ['Essentiality'],
+            adapter: {
+                type: 'Gff3TabixAdapter',
+                gffGzLocation: {
+                    uri: `${gffBaseUrl}/${genomeMeta.isolate_name}/essentiality/${genomeMeta.isolate_name}_essentiality.gff3.gz`,
                 },
-            },
-        },
-        visible: true,
-        displays: [
-            {
-                displayId: `essentiality_annotation-${genomeMeta.assembly_name}-LinearBasicDisplay`,
-                type: 'LinearBasicDisplay',
-                renderer: {
-                    type: 'SvgFeatureRenderer',
-                    color1: "jexl: (get(feature, 'essentiality_solid') == 'essential' && 'red') || " +
-                        "(get(feature, 'essentiality_solid') == 'not_essential' && 'blue') || " +
-                        "(get(feature, 'essentiality_solid').includes('liquid') && 'orange') || " +
-                        "(get(feature, 'essentiality_solid').includes('solid') && 'green') || 'gray'",
-                    labels: {
-                        name: "jexl:get(feature, 'id') + ' (' + get(feature, 'essentiality_solid') + ')'",
+                index: {
+                    location: {
+                        uri: `${gffBaseUrl}/${genomeMeta.isolate_name}/essentiality/${genomeMeta.isolate_name}_essentiality.gff3.gz.tbi`,
                     },
-
                 },
-                showLabels: true,
             },
-        ],
-    },
-];
+            visible: true,
+            displays: [
+                {
+                    displayId: `essentiality_annotation-${genomeMeta.assembly_name}-LinearBasicDisplay`,
+                    type: 'LinearBasicDisplay',
+                    renderer: {
+                        type: 'SvgFeatureRenderer',
+                        color1: "jexl: (get(feature, 'essentiality_solid') == 'essential' && 'red') || " +
+                            "(get(feature, 'essentiality_solid') == 'not_essential' && 'blue') || " +
+                            "(get(feature, 'essentiality_solid') == 'unclear' && 'yellow') || " +
+                            "(get(feature, 'essentiality_solid').includes('liquid') && 'orange') || " +
+                            "(get(feature, 'essentiality_solid').includes('solid') && 'green') || " +
+                            "(get(feature, 'essentiality_liquid').includes('liquid') && 'orange') || " +
+                            "(get(feature, 'essentiality_liquid').includes('solid') && 'green') || 'gray'",
+                        labels: {
+                            name: "jexl:get(feature, 'id') + ' (' + get(feature, 'essentiality_solid') + ')'",
+                        },
+
+                    },
+                    showLabels: true,
+                },
+            ],
+        });
+    }
+
+    return tracks;
+};
 
 export default getTracks;
