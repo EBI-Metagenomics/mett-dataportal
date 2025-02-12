52deeb0db481a0600e01a25b9047f3ea550ac5c8
vikasg <vikasg@ebi.ac.uk>
2025-02-12 07:36:46 +0000

    assembly cache - in progress

--- dataportal-app/src/components/organisms/GeneViewer/assembly.ts
+++ dataportal-app/src/components/organisms/GeneViewer/assembly.ts
@@ -1,27 +1,78 @@
 import {GenomeMeta} from "../../../interfaces/Genome";
 
-const getAssembly = (genomeMeta: GenomeMeta, fastaBaseUrl: string) => ({
-    name: genomeMeta.assembly_name,
-    sequence: {
-        type: 'ReferenceSequenceTrack',
-        trackId: 'reference',
-        adapter: {
-            type: 'BgzipFastaAdapter',
-            sequences: genomeMeta.contigs.map(contig => ({
-                name: contig.seq_id,
-                length: contig.length,
-            })),
-            fastaLocation: {
-                uri: `${fastaBaseUrl}/${genomeMeta.assembly_name}/${genomeMeta.fasta_file}.gz`,
-            },
-            faiLocation: {
-                uri: `${fastaBaseUrl}/${genomeMeta.assembly_name}/${genomeMeta.fasta_file}.gz.fai`,
-            },
-            gziLocation: {
-                uri: `${fastaBaseUrl}/${genomeMeta.assembly_name}/${genomeMeta.fasta_file}.gz.gzi`,
+class AssemblyCache {
+    private static instance: AssemblyCache;
+    private cache: Map<string, any>; // Caches assembly data by assembly name
+
+    private constructor() {
+        this.cache = new Map();
+    }
+
+    // Singleton instance
+    static getInstance(): AssemblyCache {
+        if (!AssemblyCache.instance) {
+            AssemblyCache.instance = new AssemblyCache();
+        }
+        return AssemblyCache.instance;
+    }
+
+    // Get data from cache
+    get(assemblyName: string) {
+        return this.cache.get(assemblyName);
+    }
+
+    // Set data in cache
+    set(assemblyName: string, data: any) {
+        this.cache.set(assemblyName, data);
+    }
+
+    // Clear the cache
+    clear() {
+        this.cache.clear();
+    }
+}
+
+const getAssembly = (genomeMeta: GenomeMeta, fastaBaseUrl: string) => {
+    const cache = AssemblyCache.getInstance();
+    const assemblyName = genomeMeta.assembly_name;
+
+    // Check if the assembly data is cached
+    if (cache.get(assemblyName)) {
+        console.log(`Cache hit for assembly: ${assemblyName}`);
+        return cache.get(assemblyName);
+    }
+
+    console.log(`Cache miss for assembly: ${assemblyName}. Building assembly data.`);
+
+    // Build assembly data
+    const assemblyData = {
+        name: assemblyName,
+        sequence: {
+            type: 'ReferenceSequenceTrack',
+            trackId: 'reference',
+            adapter: {
+                type: 'BgzipFastaAdapter',
+                sequences: genomeMeta.contigs.map(contig => ({
+                    name: contig.seq_id,
+                    length: contig.length,
+                })),
+                fastaLocation: {
+                    uri: `${fastaBaseUrl}/${assemblyName}/${genomeMeta.fasta_file}.gz`,
+                },
+                faiLocation: {
+                    uri: `${fastaBaseUrl}/${assemblyName}/${genomeMeta.fasta_file}.gz.fai`,
+                },
+                gziLocation: {
+                    uri: `${fastaBaseUrl}/${assemblyName}/${genomeMeta.fasta_file}.gz.gzi`,
+                },
             },
         },
-    },
-});
+    };
+
+    // Cache the assembly data
+    cache.set(assemblyName, assemblyData);
+
+    return assemblyData;
+};
 
 export default getAssembly;
