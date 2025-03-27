dfdc0601ec8a277cbf28caf8166f26b44ad9441f
vikasg11 <vikasg11@gmail.com>
2025-03-27 09:56:38 +0000

    EMG-7012 - safari browser fix

--- dataportal-app/src/components/Filters/SpeciesFilter.tsx
+++ dataportal-app/src/components/Filters/SpeciesFilter.tsx
@@ -21,16 +21,22 @@ const SpeciesFilter: React.FC<SpeciesFilterProps> = ({speciesList, selectedSpeci
             }}
         >
             {speciesList.map((species) => (
-                <label key={species.acronym}>
+                <label key={species.acronym} style={{display: 'flex', alignItems: 'center', gap: '0.4rem'}}>
                     <input
                         type="checkbox"
                         checked={selectedSpecies.includes(species.acronym)}
                         onChange={() => onSpeciesSelect(species.acronym)}
+                        style={{
+                            width: '16px',
+                            height: '16px',
+                            verticalAlign: 'middle',
+                        }}
                     />
                     <i>{species.scientific_name}</i>
                 </label>
             ))}
         </div>
+
     )
 ;
 
