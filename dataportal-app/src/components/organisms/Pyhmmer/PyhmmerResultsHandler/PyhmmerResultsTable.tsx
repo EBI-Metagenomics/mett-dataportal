887275b9dc9194e4c97fd0b1d4e128454e945c80
Vikas Gupta <vikasg@ebi.ac.uk>
2025-06-20 09:59:15 +0100

    EMG-8082 - pagination implemented

--- dataportal-app/src/components/organisms/Pyhmmer/PyhmmerResultsHandler/PyhmmerResultsTable.tsx
+++ dataportal-app/src/components/organisms/Pyhmmer/PyhmmerResultsHandler/PyhmmerResultsTable.tsx
@@ -2,7 +2,9 @@ import React, {useState} from 'react';
 import styles from './PyhmmerResultsTable.module.scss';
 import {GeneService} from '../../../../services/geneService';
 import {PyhmmerResult} from "../../../../interfaces/Pyhmmer";
+import Pagination from '@components/molecules/Pagination';
 
+const DEFAULT_PAGE_SIZE = 10;
 
 interface PyhmmerResultsTableProps {
     results: PyhmmerResult[];
@@ -12,6 +14,14 @@ interface PyhmmerResultsTableProps {
 
 const PyhmmerResultsTable: React.FC<PyhmmerResultsTableProps> = ({results, loading, error}) => {
     const [loadingIdx, setLoadingIdx] = useState<number | null>(null);
+    const [currentPage, setCurrentPage] = useState<number>(1);
+    const [pageSize, setPageSize] = useState<number>(DEFAULT_PAGE_SIZE);
+
+    const totalPages = Math.ceil((results?.length || 0) / pageSize);
+    const hasPrevious = currentPage > 1;
+    const hasNext = currentPage < totalPages;
+
+    const paginatedResults = results.slice((currentPage - 1) * pageSize, currentPage * pageSize);
 
     const handleTargetClick = async (locus_tag: string, idx: number) => {
         setLoadingIdx(idx);
@@ -28,6 +38,15 @@ const PyhmmerResultsTable: React.FC<PyhmmerResultsTableProps> = ({results, loadi
         }
     };
 
+    const handlePageClick = (page: number) => {
+        setCurrentPage(page);
+    };
+
+    const handlePageSizeChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
+        setPageSize(Number(event.target.value));
+        setCurrentPage(1);
+    };
+
     if (loading) {
         return <div className={styles.spinner}>Loading...</div>;
     }
@@ -38,6 +57,7 @@ const PyhmmerResultsTable: React.FC<PyhmmerResultsTableProps> = ({results, loadi
         return <div className={styles.noResults}>No results found.</div>;
     }
     return (
+        <>
         <table className={styles.resultsTable}>
             <thead>
             <tr>
@@ -50,7 +70,7 @@ const PyhmmerResultsTable: React.FC<PyhmmerResultsTableProps> = ({results, loadi
             </tr>
             </thead>
             <tbody>
-            {results.map((result, idx) => (
+            {paginatedResults.map((result, idx) => (
                 <tr key={idx}>
                     <td>
                         <a
@@ -76,6 +96,22 @@ const PyhmmerResultsTable: React.FC<PyhmmerResultsTableProps> = ({results, loadi
             ))}
             </tbody>
         </table>
+        {/* Page size dropdown and pagination */}
+        <div className={styles.paginationContainer}>
+
+            <div className={styles.paginationBar}>
+                {totalPages > 1 && (
+                    <Pagination
+                        currentPage={currentPage}
+                        totalPages={totalPages}
+                        hasPrevious={hasPrevious}
+                        hasNext={hasNext}
+                        onPageClick={handlePageClick}
+                    />
+                )}
+            </div>
+        </div>
+        </>
     );
 };
 
